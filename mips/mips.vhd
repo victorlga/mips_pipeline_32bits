1383d93c45a76f542848b0c3f65559feb13f0c05
Victor Assis <victorluisgassis@gmail.com>
2023-11-26 12:11:59 -0300

    Add cmts on top-level to improve its comprehension

--- mips/mips.vhd
+++ mips/mips.vhd
@@ -1,97 +1,99 @@
 library ieee;
 use ieee.std_logic_1164.all;
 
+-- Definição da entidade MIPS com parâmetros genéricos e portas de I/O.
 entity mips is
-  -- Total de bits das entradas e saidas
-  generic ( larguraDados : natural := 32;
-            simulacao : boolean := TRUE -- para gravar na placa, altere de TRUE para FALSE
+  generic (
+    larguraDados : natural := 32;  -- Define a largura dos dados manipulados.
+    simulacao : boolean := TRUE  -- Alterar para FALSE para gravação em placa.
   );
   port   (
-    CLOCK_50 : in std_logic; -- clock de 50 MHz
-	 FPGA_RESET_N : in std_logic; -- botão de reset
-    KEY : in std_logic_vector(3 downto 0); -- 4 botões
-	 SW : in std_logic_vector(9 downto 0); -- 10 chaves
+    CLOCK_50 : in std_logic;  -- Clock de entrada de 50 MHz.
 	 
-	 LEDR : out std_logic_vector(9 downto 0); -- 10 leds
-	
-	-- 6 displays de 7 segmentos
-	--ROM_AddressOUT : out std_logic_vector(addrWidth - 1 downto 0);
-	 HEX0: out std_logic_vector(6 downto 0);
-	 HEX1: out std_logic_vector(6 downto 0);
-	 HEX2: out std_logic_vector(6 downto 0);
-	 HEX3: out std_logic_vector(6 downto 0);
-	 HEX4: out std_logic_vector(6 downto 0);
-	 HEX5: out std_logic_vector(6 downto 0);
+    FPGA_RESET_N : in std_logic;  -- Sinal de reset.
+    KEY : in std_logic_vector(3 downto 0);  -- Botões de entrada.
 	 
-	 saidaMUXDisplay : out std_logic_vector(larguraDados-1 downto 0);
-	 saidaROM : out std_logic_vector (larguraDados-1 downto 0)
+    SW : in std_logic_vector(9 downto 0);  -- Chaves de entrada.
+    
+    LEDR : out std_logic_vector(9 downto 0);  -- LEDs de saída.
+    
+    -- Displays de 7 segmentos.
+    HEX0: out std_logic_vector(6 downto 0);
+    HEX1: out std_logic_vector(6 downto 0);
+    HEX2: out std_logic_vector(6 downto 0);
+    HEX3: out std_logic_vector(6 downto 0);
+    HEX4: out std_logic_vector(6 downto 0);
+    HEX5: out std_logic_vector(6 downto 0);
+	 
+	 -- Saida do MUX utilizado para visualizar o funcionamento do programa
+	 saidaMUXDisplay : out std_logic_vector(larguraDados-1 downto 0)
   );
 end entity;
 
 architecture arquitetura of mips is
 
-	signal CLK : std_logic;
+	signal CLK : std_logic;														-- Define sinal de clock utilizado na CPU
 	
-	signal sinal_controle : std_logic_vector (8 downto 0);
+	signal sinal_controle : std_logic_vector (8 downto 0);						-- Define o sinal de controle da CPU, após decodificar a instrução
 	
-	signal proxEnd : std_logic_vector (larguraDados-1 downto 0);
-	signal Endereco : std_logic_vector (larguraDados-1 downto 0);
-	signal EndMais4 : std_logic_vector (larguraDados-1 downto 0);
-	signal EndMais4MaisImShft : std_logic_vector (larguraDados-1 downto 0);
-	signal SigExtIm : std_logic_vector (larguraDados-1 downto 0);
-	signal SigExtImShft : std_logic_vector (larguraDados-1 downto 0);
+	signal proxEnd : std_logic_vector (larguraDados-1 downto 0);				-- Define o próximo endereço a ser executado
+	signal Endereco : std_logic_vector (larguraDados-1 downto 0);				-- Define o endereço atual
+	signal EndMais4 : std_logic_vector (larguraDados-1 downto 0);				-- Define o endereço atual + 4
+	signal EndMais4MaisImShft : std_logic_vector (larguraDados-1 downto 0);		-- Define o endereço atual + 4 + Imediato shiftado
+	signal SigExtIm : std_logic_vector (larguraDados-1 downto 0);				-- Define o sinal de extensão de sinal do imediato
+	signal SigExtImShft : std_logic_vector (larguraDados-1 downto 0);			-- Define o sinal de extensão de sinal do imediato shiftado
 	
-	signal zeroANDbeq : std_logic;
+	signal zeroANDbeq : std_logic;												-- Define o sinal de zero AND beq
 	
-	signal zero : std_logic;
-	signal ULASaida : std_logic_vector (larguraDados-1 downto 0);
+	signal zero : std_logic;													-- Define sinal que indica se a saída da ULA é zero
+	signal ULASaida : std_logic_vector (larguraDados-1 downto 0);				-- Define sinal de saída da ULA
 	
-	signal ROMsaida : std_logic_vector (larguraDados-1 downto 0);
+	signal ROMsaida : std_logic_vector (larguraDados-1 downto 0);				-- Define sinal de saída da ROM
 	
-	signal endReg3 : std_logic_vector (4 downto 0);
+	signal endReg3 : std_logic_vector (4 downto 0);								-- Define sinal de endereço do registrador 3 (RT ou RD)
 	
-	signal opcode 	 : std_logic_vector (5 downto 0);
-	signal endRegRS : std_logic_vector (4 downto 0);
-	signal endRegRT : std_logic_vector (4 downto 0);
-	signal endRegRD : std_logic_vector (4 downto 0);
-	signal shant 	 : std_logic_vector (4 downto 0);
-	signal funct  	 : std_logic_vector (5 downto 0);
+	signal opcode 	 : std_logic_vector (5 downto 0);							-- Define sinal de opcode que vem da ROM
+	signal endRegRS : std_logic_vector (4 downto 0);							-- Define sinal de endereço do registrador RS
+	signal endRegRT : std_logic_vector (4 downto 0);							-- Define sinal de endereço do registrador RT
+	signal endRegRD : std_logic_vector (4 downto 0);							-- Define sinal de endereço do registrador RD
+	signal shant 	 : std_logic_vector (4 downto 0);							-- Define sinal de shant que vem da ROM
+	signal funct  	 : std_logic_vector (5 downto 0);							-- Define sinal de funct que vem da ROM
 	
-	signal Imediato26 : std_logic_vector (25 downto 0);
-	signal Imediato26Shft : std_logic_vector (27 downto 0);
+	signal Imediato26 : std_logic_vector (25 downto 0);							-- Define o sinal dos 26 bits menos significativos do imediato
+	signal Imediato26Shft : std_logic_vector (27 downto 0);						-- Define o sinal dos 26 bits menos significativos do imediato shiftado
 	
-	signal Imediato16 : std_logic_vector (15 downto 0);
+	signal Imediato16 : std_logic_vector (15 downto 0);							-- Define o sinal dos 16 bits menos significativos do imediato
 	
 	
-	signal entradaAMuxProxPC : std_logic_vector (larguraDados-1 downto 0);
-	signal entradaBMuxProxPC : std_logic_vector (larguraDados-1 downto 0);
-	signal selMuxProxPC : std_logic;
+	signal entradaAMuxProxPC : std_logic_vector (larguraDados-1 downto 0);		-- Define o sinal de entrada A do MUX do próximo endereço
+	signal entradaBMuxProxPC : std_logic_vector (larguraDados-1 downto 0);		-- Define o sinal de entrada B do MUX do próximo endereço
+	signal selMuxProxPC : std_logic;											-- Define o sinal de seleção do MUX do próximo endereço
 	
-	signal selMuxRTRD : std_logic;
-	signal habEscritaReg : std_logic;
+	signal selMuxRTRD : std_logic;												-- Define o sinal de seleção do MUX do registrador 3
+	signal habEscritaReg : std_logic;											-- Define o sinal de habilitação de escrita do registrador 3
 	
-	signal dadoEscritaReg3 : std_logic_vector (larguraDados-1 downto 0);
-	signal dadoLidoReg1 : std_logic_vector (larguraDados-1 downto 0);
-	signal dadoLidoReg2 : std_logic_vector (larguraDados-1 downto 0);
-	signal entradaB_ULA : std_logic_vector (larguraDados-1 downto 0);
+	signal dadoEscritaReg3 : std_logic_vector (larguraDados-1 downto 0);		-- Define o sinal de dado de escrita do registrador 3
+	signal dadoLidoReg1 : std_logic_vector (larguraDados-1 downto 0);			-- Define o sinal de dado lido do registrador 1
+	signal dadoLidoReg2 : std_logic_vector (larguraDados-1 downto 0);			-- Define o sinal de dado lido do registrador 2
+	signal entradaB_ULA : std_logic_vector (larguraDados-1 downto 0);			-- Define o sinal de entrada B da ULA
 	
-	signal selMuxRegSig : std_logic;
-	signal selMuxULARAM : std_logic;
-	signal beq : std_logic;
-	signal tipoR : std_logic;
+	signal selMuxRegSig : std_logic;											-- Define o sinal de seleção do MUX do registrador 2 e sinal extendido
+	signal selMuxULARAM : std_logic;											-- Define o sinal de seleção do MUX da ULA e RAM para ir para o registrador 3
+	signal beq : std_logic;														-- Define o sinal de beq que vem da decodificação da saida da ROM
+	signal tipoR : std_logic;													-- Define o sinal de tipoR que vem da decodificação da saida da ROM
 	
-	signal ULActrl : std_logic_vector(3 downto 0);
+	signal ULActrl : std_logic_vector(3 downto 0);								-- Define o sinal de controle da ULA
 	
-	signal RAMsaida : std_logic_vector (larguraDados-1 downto 0);
-	signal habEscritaRAM : std_logic;
-	signal habLeituraRAM : std_logic;
+	signal RAMsaida : std_logic_vector (larguraDados-1 downto 0);				-- Define o sinal de saída da RAM
+	signal habEscritaRAM : std_logic;											-- Define o sinal de habilitação de escrita da RAM
+	signal habLeituraRAM : std_logic;											-- Define o sinal de habilitação de leitura da RAM
 	
-	-- signal entrada_hex0,entrada_hex1,entrada_hex2,entrada_hex3,entrada_hex4,entrada_hex5: std_logic_vector(6 downto 0);
-	
-	-- signal saidaMUXDisplay: std_logic_vector(larguraDados-1 downto 0);
+	-- Define os sinais utilizados para testar o funcionamento da CPU.
+	signal entrada_hex0,entrada_hex1,entrada_hex2,entrada_hex3,entrada_hex4,entrada_hex5: std_logic_vector(6 downto 0);
+	signal saidaMUXDisplay: std_logic_vector(larguraDados-1 downto 0);
 	
 begin
-
+	-- Define o clock da CPU.
 	gravar:  if simulacao generate
 		CLK <= KEY(0);
 	else generate
@@ -99,6 +101,7 @@ begin
 			  port map (clk => CLOCK_50, entrada => (not KEY(0)), saida => CLK);
 	end generate;
 
+	-- Define a entidade que implementa o program counter.
 	PROGRAM_COUNTER : entity work.registradorGenerico
 		generic map (larguraDados => larguraDados)
 		port map (DIN => proxEnd,
@@ -107,18 +110,21 @@ begin
 					 CLK => CLK,
 					 RST => '0');
 	
+	-- Define a entidade que implementa a soma de 4 ao endereço atual.
 	SOMA_CONST_4 : entity work.somaConstante
 		generic map (larguraDados => larguraDados,
 						 constante => 4)
 		port map (entrada => Endereco,
 					 saida => EndMais4);
 	
+	-- Define a entidade que implementa a soma do endereço atual + 4 com o imediato shiftado.
 	SOMA_PC_SIG : entity work.somadorGenerico
 		generic map (larguraDados => larguraDados)
 		port map (entradaA => EndMais4,
 					 entradaB => SigExtImShft,
 					 saida => EndMais4MaisImShft);
 
+	-- Define a entidade que implementa o banco de registradores.
 	BLOCO_REGISTRADORES : entity work.bancoRegGenerico
 		generic map (larguraDados => larguraDados,
 						 larguraEndBancoRegs => 5)
@@ -130,7 +136,8 @@ begin
 					 escreveC => habEscritaReg,
 					 saidaA => dadoLidoReg1,
 					 saidaB => dadoLidoReg2);
-					  
+	
+	-- Define a entidade que implementa a ULA.
 	ULA : entity work.ULAMIPS
 		generic map (larguraDados => larguraDados)
       port map (entradaA => dadoLidoReg1,
@@ -138,14 +145,16 @@ begin
 					 saida => ULASaida,
 					 zero => zero,
 					 seletor => ULActrl);
-					 
+	
+	-- Define a entidade que implementa a ROM.
 	ROM : entity work.ROMMIPS
 		generic map (dataWidth => larguraDados,
 						 addrWidth => larguraDados,
 						 memoryAddrWidth => 6)
 		port map (Endereco => Endereco,
 					 Dado => ROMsaida);
-					 
+	
+	-- Define a entidade que implementa a RAM.
 	RAM : entity work.RAMMIPS
 		generic map(dataWidth => larguraDados,
 						addrWidth => larguraDados,
@@ -157,7 +166,8 @@ begin
 					re => habLeituraRAM,
 					habilita => '1',
 					Dado_out => RAMsaida);
-					 
+	
+	-- Define a entidade que implementa o MUX do registrador 3.
 	MUX_RT_RD : entity work.muxGenerico2x1
 		generic map (larguraDados => 5)
 		port map (entradaA_MUX => endRegRT,
@@ -165,57 +175,70 @@ begin
                  seletor_MUX => selMuxRTRD,
                  saida_MUX => endReg3);
 	
+	-- Define a entidade que implementa o MUX que seleciona entre
+	-- o endereço atual + 4 e o endereço atual + 4 + Imediato shiftado.
 	MUX_PC_SIGEXT : entity work.muxGenerico2x1
 		generic map (larguraDados => larguraDados)
 		port map (entradaA_MUX => EndMais4,
                  entradaB_MUX => EndMais4MaisImShft,
                  seletor_MUX => zeroANDbeq,
                  saida_MUX => entradaAMuxProxPC);
-					  
+	
+	-- Define a entidade que implementa o MUX que seleciona entre o 
+	-- dado lido do registrador 2 e o imediato com sinal extendido.
 	MUX_REG_SIGEXT : entity work.muxGenerico2x1
 		generic map (larguraDados => larguraDados)
 		port map (entradaA_MUX => dadoLidoReg2,
                  entradaB_MUX => SigExtIm,
                  seletor_MUX => selMuxRegSig,
                  saida_MUX => entradaB_ULA);
-					  
+	
+	-- Define a entidade que implementa o MUX que seleciona entre o
+	-- dado lido da RAM e a saída da ULA.
 	MUX_ULA_RAM : entity work.muxGenerico2x1
 		generic map (larguraDados => larguraDados)
 		port map (entradaA_MUX => ULASaida,
                  entradaB_MUX => RAMsaida,
                  seletor_MUX => selMuxULARAM,
                  saida_MUX => dadoEscritaReg3);
-					  
+	
+	-- Define a entidade que implementa o MUX que seleciona entre a
+	-- saida do MUX_PC_SIGEXT e o endereco + 4 + imediato shiftado.
 	MUX_PROX_PC : entity work.muxGenerico2x1
 		generic map (larguraDados => larguraDados)
 		port map (entradaA_MUX => entradaAMuxProxPC,
                  entradaB_MUX => entradaBMuxProxPC,
                  seletor_MUX => selMuxProxPC,
                  saida_MUX => proxEnd);
-					  
+	
+	-- Define a entidade que implementa a extensão de sinal do
+	-- imediato de 16 bits.
 	ESTENDE_SIN_IM : entity work.estendeSinalGenerico
 		generic map (larguraDadoEntrada => 16,
 						 larguraDadoSaida => larguraDados)
 		port map(estendeSinal_IN => Imediato16,
 					estendeSinal_OUT => SigExtIm);
 	
+	-- Define a entidade que implementa a unidade de controle de dados
 	UNID_CONTROLE_DADOS : entity work.unidadeControleDados
 		port map (opcode => opcode,
 					 sinal_controle => sinal_controle);
-					 
+
+	-- Define a entidade que implementa a unidade de controle da ULA
 	UNID_CONTROLE_ULA : entity work.unidadeControleULA
 		port map (opcode => opcode,
 					 funct => funct,
 					 tipoR => tipoR,
 					 ULActrl => ULActrl);
-					 
+	
+	-- Define as entidades utilizadas para testar o funcionamento da CPU.
 	MUX_DISPLAY :  entity work.muxGenerico2x1
 		generic map (larguraDados => larguraDados)
       port map( entradaA_MUX => Endereco,
                 entradaB_MUX => dadoEscritaReg3,
                 seletor_MUX => SW(0), 
                 saida_MUX => saidaMUXDisplay);
-	/*	 
+	
 	HEX_0 : entity work.displayHEX
 				 port map (	Data_IN => saidaMUXDisplay(3 downto 0),
 								Entrada_HEX => entrada_hex0);
@@ -234,8 +257,9 @@ begin
 	HEX_5 : entity work.displayHEX
 				 port map (	Data_IN => saidaMUXDisplay(23 downto 20),
 								Entrada_HEX => entrada_hex5);
-	*/
+	-- Termina a definição das entidades utilizadas para testar o funcionamento da CPU.
 
+	-- Preenche os sinais da CPU com seus respectivos valores.
 	SigExtImShft  		<= SigExtIm(29 downto 0) & "00";
 	zeroANDbeq	  		<= beq and zero;
 	
@@ -267,7 +291,8 @@ begin
 	
 	saidaROM <= ROMsaida;
 	
-	/*
+	
+	-- Preeche as saídas do top-level da CPU utilizadas para testar o funcionamento.
 	HEX0 <= entrada_hex0;
 	HEX1 <= entrada_hex1;
 	HEX2 <= entrada_hex2;
@@ -278,7 +303,6 @@ begin
 	
 	LEDR(3 downto 0) <= saidaMUXDisplay(27 downto 24);
 	LEDR(7 downto 4) <= saidaMUXDisplay(31 downto 28);
-	*/
-	
+
 
 end architecture;
